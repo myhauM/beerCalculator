ee2aef9a6384d5893ae8dbb1cc266bb252ea02fc
myhau <myhau16@gmail.com>
2020-04-02 18:43:02 +0200

    Initial Commit

---
+++ beerCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  beerCalculator
+//
+//  Created by Michał Massloch on 02/04/2020.
+//  Copyright © 2020 Michał Massloch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
