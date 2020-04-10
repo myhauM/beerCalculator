43adf093fc8bbc035b034fb94197089e0ddba5cf
myhau <myhau16@gmail.com>
2020-04-10 19:43:44 +0200

    Dodany IQKeyboard, kropka zamiast przecinka w klawiaturze

---
+++ beerCalculator/Extensions/String+CommaToDot.swift
@@ -0,0 +1,9 @@
+//
+//  String+CommaToDot.swift
+//  beerCalculator
+//
+//  Created by Michał Massloch on 10/04/2020.
+//  Copyright © 2020 Michał Massloch. All rights reserved.
+//
+
+import Foundation
